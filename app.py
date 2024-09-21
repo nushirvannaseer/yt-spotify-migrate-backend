b26bf5be67f70ecaf182aa87701da04469df63eb
Nushirvan Naseer <nushirvannaseer@gmail.com>
2024-09-21 20:28:00 +0500

    port update

--- app.py
+++ app.py
@@ -41,7 +41,6 @@ def logout():
 
 
 # Run the Flask application
-
-
 if __name__ == '__main__':
-    app.run(port=int(os.getenv("PORT")), debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
